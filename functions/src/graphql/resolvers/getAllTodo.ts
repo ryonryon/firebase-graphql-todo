27d8b1cc5907ad3eef034fe08761bfed6a1b26bd
ryotogashi <ryo.togashi.ca@gmail.com>
2019-11-09 11:13:11 -0800

    add firestoreTodo finish getAllTodo.

--- functions/src/graphql/resolvers/getAllTodo.ts
+++ functions/src/graphql/resolvers/getAllTodo.ts
@@ -1,9 +1,31 @@
 import firestore from "../../singleton/firestore";
 import Todo from "../../entities/Todo";
 
-async function getAllTodo() {
+async function getAllTodo(): Promise<Todo[]> {
   const todos = await firestore.collection("todos").get();
-  return todos.docs.map(works => works.data()) as Todo[];
+  return todos.docs.map(todo => {
+    const rawTodo = todo.data();
+    return new firestoreTodo(
+      todo.id,
+      rawTodo.title,
+      rawTodo.content,
+      rawTodo.isDone
+    );
+  });
+}
+
+class firestoreTodo implements Todo {
+  id: string;
+  title: string;
+  content: string;
+  isDone: boolean;
+
+  constructor(id: string, title: string, content: string, isDone: boolean) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.isDone = isDone;
+  }
 }
 
 export default getAllTodo;
